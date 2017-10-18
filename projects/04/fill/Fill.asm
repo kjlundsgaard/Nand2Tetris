738031e8a15584911a2efcfc8125b57b61a8d1ae
kjlundsgaard <katie.lundsgaard@gmail.com>
2017-10-18 14:36:00 -0400

    write assembly programs to multiply two numbers and fill a screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,59 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @KBD
+    D=A
+    @max
+    M=D
+    @SCREEN
+    D=A
+    @R0
+    M=D //PUT SCREEN START LOCATION IN RAM0
+
+(CHECK_KEY)
+    @KBD
+    D=M
+    @FILL_LOOP
+    D;JGT
+    @ERASE_LOOP
+    D;JEQ
+
+(FILL_LOOP)
+    @R0
+    A=M //GET ADRESS OF SCREEN PIXEL TO FILL
+    M=-1 //FILL IT WITH BLACK
+
+    @R0
+    MD=M+1   //INC TO NEXT PIXEL
+    @max
+    D=M-D   //KBD-SCREEN set D to kbd index minus current index
+
+    @FILL_LOOP
+    D;JGT   //IF A=0 (at end of screen idx) EXIT AS THE WHOLE SCREEN IS BLACK
+
+    @LOOP
+    0;JMP // infinite loop
+
+(ERASE_LOOP)
+    @R0
+    A=M //GET ADRESS OF SCREEN PIXEL TO FILL
+    M=0 //FILL IT WITH WHITE
+
+    @R0
+    MD=M+1   //INC TO NEXT PIXEL
+    @max
+    D=M-D   //KBD-SCREEN set D to kbd index minus current index
+
+    @ERASE_LOOP
+    D;JGT   //IF A=0 (at end of screen idx) EXIT AS THE WHOLE SCREEN IS WHITE
+
+    @LOOP
+    0;JMP // infinite loop
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Pseudo code
+// take memory at R0, loop through it R1 times, add value at R2 to it R1 times
+// (set n to value at R1 and then subtract by 1, jump to end if 0)
+
+    @R1
+    D=M
+    @n
+    M=D // set variable n to value of memory 1
+
+    @R2
+    M=0 // initialize product to 0
+
+(LOOP)
+    @R0
+    D=M // get value at memory address 0
+
+    @R2
+    M=D+M // increment address 2 by value at address 0
+
+    @n
+    M=M-1
+    D=M
+    @END
+    D;JEQ // go to end if n is 0
+
+    @LOOP
+    0;JMP // go to LOOP
+
+(END)
+    @END
+    0;JMP // infinite loop to end program
